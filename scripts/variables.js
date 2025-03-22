3dc68e378eee6ba1d780d24a6a3e93aaf671698c
jhs <ingjavierhernsan@hotmail.com>
2025-03-22 03:24:47 -0700

    Exercise varaibles

---
+++ scripts/variables.js
@@ -0,0 +1,36 @@
+a = 1;
+b = 2;
+c = a + b;
+
+document.getElementById("spot00").innerText = c;
+
+var d = 3;
+var e = 4;
+var f = d + e;
+
+document.getElementById("spot01").innerText = f;
+
+let g = 5;
+let h = 6;
+let i = g + h;
+
+document.getElementById("spot02").innerText = i;
+
+const x = 7;
+const y = 8;
+const z = x + y;
+
+document.getElementById("spot03").innerText = z;
+
+let person = "John Doe";
+
+document.getElementById("spot03").innerText = person;
+
+let carName; //undefined
+
+document.getElementById("spot04").innerText = carName;
+
+let concat00 = "5" + 2 + 3;
+let concat01 = 2 + 3 + "5";
+
+document.getElementById("spot05").innerText = "concat00: " + concat00 + " " + "concat01: " + concat01;
